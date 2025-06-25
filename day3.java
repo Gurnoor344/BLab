3ab651a7fc80806ae8be8d739d5ec40941534622
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-06-25 15:07:51 +0530

    third commit

---
+++ day3.java
@@ -0,0 +1,20 @@
+class hello{
+    public static void main(String[] args){
+        System.out.println("HIIIIIIIIIII");
+
+        int a=10;
+        int b=30;
+        int c=a+b;
+        System.out.println(c);
+
+        int d=a-b;
+        System.out.println(d);
+
+        int e=a/b;
+        System.out.println(e);
+
+        int f=a*b;
+        System.out.println(f);
+    
+    }
+}
